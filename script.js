b36a2c0d40d41ce00984f3a23858daa99fec6e44
keinyrivas <67443784+keinyrivas@users.noreply.github.com>
2020-07-22 22:36:07 -0300

    commit dois

---
+++ script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        prompt("Qual é seu nome?")
+    </script>
+    <script>
+        prompt("Qual é sua idade?")
+    </script>
+    <script>
+        prompt("Qual é seu pais de Nascimento?")
+    </script>
+    <script>
+        prompt("Qual é seu genero?")
+    </script>
+</body>
+</html>
